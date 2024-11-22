471c5891379792c6e029f14687124bd5579406b5
Janita Willumsen <janitaws@icloud.com>
2024-11-22 14:06:39 +0100

    Add plot ninja stuff

--- src/pthree/__init__.py
+++ src/pthree/__init__.py
@@ -5,11 +5,11 @@
 import importlib.metadata
 
 # imports
-
+from .plot import set_plt_params
 
 __version__ = importlib.metadata.version(__package__)
 
 # add function names
 __all__ = [
-
+    "set_plt_params"
 ]
