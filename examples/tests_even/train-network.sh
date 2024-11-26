56fd15820f77a74c6e5b14e3fe3dbbac70bb1654
evengar <evengar@uio.no>
2024-11-26 13:41:40 +0100

    reduce wall time to shorten waiting

--- examples/tests_even/train-network.sh
+++ examples/tests_even/train-network.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=TestCNN
 #SBATCH --account=ec16
-#SBATCH --time=3:00:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2G
