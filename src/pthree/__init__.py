6967d96ab940683f5c0144cfafe7d0c264ec9197
Janita Willumsen <janitaws@icloud.com>
2024-12-07 10:06:29 +0100

    Adds dino feature selection to module

--- src/pthree/__init__.py
+++ src/pthree/__init__.py
@@ -7,7 +7,7 @@ import importlib.metadata
 # imports
 from .plot import set_plt_params
 from .image_utils import img_label_from_folder, split_imagedata, train_cnn, ImageDataset
-from .create_dataset import create_full_dataset, feature_selection_ecotaxa
+from .create_dataset import create_full_dataset, feature_selection_ecotaxa, feature_selection_dino, feature_selection_dino_pca
 
 __version__ = importlib.metadata.version(__package__)
 
@@ -19,5 +19,7 @@ __all__ = [
     "train_cnn",
     "ImageDataset",
     "create_full_dataset",
-    "feature_selection_ecotaxa"
+    "feature_selection_ecotaxa", 
+    "feature_selection_dino", 
+    "feature_selection_dino_pca"
 ]
